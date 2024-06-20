cd24f66c47510a56634e3352c36db9629bf16406
David Clarke <david_anthony_clarke@hotmail.com>
2024-06-20 13:29:58 +1000

    Update 00_main_menu.R

--- R/00_main_menu.R
+++ R/00_main_menu.R
@@ -20,3 +20,28 @@ pth <- "C:/Users/dcla0021/Documents/postdoc/projects/sampling_plan/data"
 
 ## User defined functions
 source("R/01_functions.R")
+
+## Prepare soil data
+source("R/02_soil_data.R")
+
+## Interpolate soil data
+source("R/03_interpolate_soil_data.R")
+
+## Data preparation for stratification
+source("R/04_data_preparation.R")
+
+## Create classification units
+source("R/05_classification_units.R")
+
+## Create stratification units
+source("R/06_stratification_units.R")
+
+## Create spatial signatures
+source("R/07_spatial_signatures.R")
+
+## Create landscape similarity
+source("R/08_landscape_similarity.R")
+
+## Find plot locations and samples within, following constraints
+source("R/09_plot_locations.R")
+
